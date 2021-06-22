a3a521f567ede79898a49d5874ad9d0e01cdd5cb
ucwealth <ucheokeke16@gmail.com>
2021-06-22 13:07:45 +0100

    added comment

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,14 @@ class MyApp extends StatelessWidget {
         home: RandomWords());
   }
 }
+
+/*
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[900]),
+        home: RandomWords());
+  }
+}
+*/
